e5a4df59ce1fcd96c5a7ab3a955b971f61c3bb3b
William McGiffert <williammcgiffert@Williams-MacBook-Pro.local>
2024-01-25 09:25:38 -0600

    Inital Commit

---
+++ src/routes/boat/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<h1>{data.boatName}</h1>
+
+<form method="post" action="?/rename" use:enhance>
+  <input type="text" name="boatName">
+  <button type="submit">Submit</button>
+  <button formaction="?/capitalize">Capitalize</button>
+</form>
