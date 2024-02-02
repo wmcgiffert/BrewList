e35cfbd104a1c4e21912d9320624a4a28686708f
wmcgiffert <wmcgiffert@crimson.ua.edu>
2024-02-02 15:27:31 -0600

    Setting Up Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
-<h1 class=text-green-50 >Welcome to SvelteKit</h1>
+<h1 class="text-green-400 font-bold underline">Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 <p>Hi Mom!</p>
 <a href="/boat">Go To Boat</a>
 
+<style lang="postcss">
+    :global(html) {
+        background-color: #636262;
+    }
+</style>
